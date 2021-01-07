af6c87ad0ff9c2e00690647083de6e8d70cb33b2
wuyurong <wuyurong@jsbdshop.cn>
2021-01-07 15:03:09 +0800

    的士速递

--- ts/类/类.js
+++ ts/类/类.js
@@ -1,26 +1,46 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Animal = /** @class */ (function () {
-    function Animal(theName) {
-        this.name = theName;
+// class Animal {
+//     public name: string;
+//     protected protectedN: string;
+//     private privateN: string;
+//     constructor(theName: string) { this.name = theName; }
+// }
+// class Dog extends Animal {
+//   constructor () {
+//     super('1')
+//   }
+// }
+/**
+private 的兼容性
+ */
+// class Animal {
+//   private name: string;
+//   constructor(theName: string) { this.name = theName; }
+// }
+// class Rhino extends Animal {
+//   constructor() { super("Rhino"); }
+// }
+// class Employee {
+//   private name: string;
+//   constructor(theName: string) { this.name = theName; }
+// }
+// let animal = new Animal("Goat");
+// let rhino = new Rhino();
+// let employee = new Employee("Bob");
+// animal = rhino;
+// animal = employee; // 错误: Animal 与 Employee 不兼容.
+/**
+class Person {
+    protected name: string;
+    protected constructor(theName: string) { this.name = theName; }
+}
+protected的构造函数不可以被new 但是可以被继承
+参数属性改造
+ */
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
     }
-    return Animal;
+    ;
+    return Person;
 }());
-var Dog = /** @class */ (function (_super) {
-    __extends(Dog, _super);
-    function Dog() {
-        return _super.call(this, '1') || this;
-    }
-    return Dog;
-}(Animal));
+var ad = new Person("1111");
--- ts/类/类.ts
+++ ts/类/类.ts
@@ -1,12 +1,52 @@
+// class Animal {
+//     public name: string;
+//     protected protectedN: string;
+//     private privateN: string;
+//     constructor(theName: string) { this.name = theName; }
+// }
+
+// class Dog extends Animal {
+//   constructor () {
+//     super('1')
+//   }
+// }
+
+//? private的兼容性问题
+/**
+ *
 class Animal {
-    public name: string;
-    protected protectedN: string;
-    private privateN: string;
-    constructor(theName: string) { this.name = theName; }
+  private name: string;
+  constructor(theName: string) { this.name = theName; }
 }
 
-class Dog extends Animal {
-  constructor () {
-    super('1')
-  }
+class Rhino extends Animal {
+  constructor() { super("Rhino"); }
 }
+
+class Employee {
+  private name: string;
+  constructor(theName: string) { this.name = theName; }
+}
+
+let animal = new Animal("Goat");
+let rhino = new Rhino();
+let employee = new Employee("Bob");
+
+animal = rhino;
+animal = employee; // 错误: Animal 与 Employee 不兼容.
+
+*/
+
+// ? protected的构造函数不可以被new 但是可以被继承
+/**
+class Person {
+    protected name: string;
+    protected constructor(theName: string) { this.name = theName; }
+}
+// ? 参数属性改造(默认赋值)
+class Person {
+  constructor(readonly name:string){};
+}
+var ad:Person = new Person("1111")
+ */
+
