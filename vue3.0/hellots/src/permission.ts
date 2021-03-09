1cc6e627829407931b7bf490522cc1519969c7f9
DESKTOP-JFRCGUH\Admin <1065229722@qq.com>
2021-03-09 23:51:46 +0800

    111

--- vue3.0/hellots/src/permission.ts
+++ vue3.0/hellots/src/permission.ts
@@ -1,7 +1,9 @@
 import router from '@/router'
-console.log(router)
+import store from '@/store/index'
+
 
 router.beforeEach((to: any, from: any, next: Function) => {
     console.log(from, to)
+    console.log(store)
     next()
 })
