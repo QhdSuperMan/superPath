b4b7e7ff984622b194276ce78392e8b6259f4d07
wuyurong <wuyurong@jsbdshop.cn>
2021-03-16 17:14:23 +0800

    自动测试

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ require('shelljs/global');
 
 function gitAction() {
   exec('git add .', function (status, output) {
+    console.log('添加代码到版本控制')
     exec('git commit -m "自动测试"', function (status, output) {
+      console.log('提交到本地缓存区')
       exec('git pull origin master', function (status, output) {
+        console.log('拉代码')
         exec('git push origin master', function (status, output) {
-
+          console.log('推代码')
         });
       });
     });
