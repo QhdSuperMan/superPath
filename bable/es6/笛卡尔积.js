2e56122bc16883722c21519a440e84d550a7c537
wuyurong <wuyurong@jsbdshop.cn>
2021-06-10 10:30:02 +0800

    提交

---
+++ bable/es6/笛卡尔积.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: 吴玉荣
+ * @LastEditors: 吴玉荣
+ * @Date: 2021-05-25 19:08:50
+ * @LastEditTime: 2021-05-25 19:35:21
+ * @info: 描述
+ */
+function cartesian(arr) {
+    if (arr.length < 2) return arr[0] || [];
+    return [].reduce.call(arr, function (col, set) {
+        let res = [];
+        col.forEach(c => {
+            set.forEach(s => {
+                let t = [].concat(Array.isArray(c) ? c : [c]);
+                t.push(s);
+                res.push(t);
+            })
+        });
+        return res;
+    });
+}
