1188d7fd0a7baba8d3af5e299fed8033c3eb36c1
wuyurong <wuyurong@jsbdshop.cn>
2021-10-25 09:23:27 +0800

    自动测试

--- commonpack/hellop/babel.config.js
+++ commonpack/hellop/babel.config.js
@@ -8,7 +8,12 @@ module.exports = {
       {
         "libraryName": "commonp",
         "styleLibraryName": "style"
-      }
-    ]
+      },
+      // "component",
+      // {
+      //   "libraryName": "element-ui",
+      //   "styleLibraryName": "theme-chalk"
+      // }
+    ],
   ]
 }
