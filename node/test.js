82a76a17d112dc43ade0bd511d342b0ab991c0c3
wuyurong <wuyurong@jsbdshop.cn>
2021-12-10 10:01:37 +0800

    自动测试

--- node/test.js
+++ node/test.js
@@ -1,9 +1,44 @@
 /*
  * @Author: 吴玉荣
  * @LastEditors: 吴玉荣
- * @Date: 2021-10-31 18:49:01
- * @LastEditTime: 2021-10-31 18:50:38
+ * @Date: 2021-12-06 14:15:32
+ * @LastEditTime: 2021-12-08 11:29:03
  * @info: 描述
  */
-const c = require('./test1')
-console.log(c)
+console.log('1')
+setTimeout(function() {
+    console.log('2')
+    process.nextTick(function() {
+        console.log('3')
+    })
+    new Promise(function(resolve) {
+        console.log('4')
+        resolve()
+    }).then(function() {
+        console.log('5')
+    })
+})
+
+process.nextTick(function() {
+    console.log('6')
+})
+
+new Promise(function(resolve) {
+    console.log('7')
+    resolve()
+}).then(function() {
+    console.log('8')
+})
+
+setTimeout(function() {
+    console.log('9')
+    process.nextTick(function() {
+        console.log('10')
+    })
+    new Promise(function(resolve) {
+        console.log('11')
+        resolve()
+    }).then(function() {
+        console.log('12')
+    })
+})
