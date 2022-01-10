32b55b6e4d59e5cbfd1b9b3c394ff3baa1742311
wuyurong <wuyurong@jsbdshop.cn>
2022-01-10 10:07:54 +0800

    自动测试

--- vue/vuetwo/src/router/index.js
+++ vue/vuetwo/src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: 吴玉荣
  * @LastEditors: 吴玉荣
  * @Date: 2021-12-22 15:29:30
- * @LastEditTime: 2021-12-22 15:57:05
+ * @LastEditTime: 2022-01-06 10:21:11
  * @info: 描述
  */
 import Vue from 'vue'
@@ -32,7 +32,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/virtualList/virtualList.vue')
-  }
+  },
+  {
+    path: '/model',
+    name: 'model',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/model/model.vue')
+  },
 ]
 
 const router = new VueRouter({
