263161054a6f17ae2bc0b9321749cfb617d75d9a
wuyr <1065229722@qq.com>
2022-02-13 16:28:54 +0800

    自动测试

--- node/test.js
+++ node/test.js
@@ -1,44 +1,8 @@
-/*
- * @Author: 吴玉荣
- * @LastEditors: 吴玉荣
- * @Date: 2021-12-06 14:15:32
- * @LastEditTime: 2021-12-08 11:29:03
- * @info: 描述
- */
-console.log('1')
-setTimeout(function() {
-    console.log('2')
-    process.nextTick(function() {
-        console.log('3')
-    })
-    new Promise(function(resolve) {
-        console.log('4')
-        resolve()
-    }).then(function() {
-        console.log('5')
-    })
-})
 
-process.nextTick(function() {
-    console.log('6')
-})
-
-new Promise(function(resolve) {
-    console.log('7')
-    resolve()
-}).then(function() {
-    console.log('8')
-})
-
-setTimeout(function() {
-    console.log('9')
-    process.nextTick(function() {
-        console.log('10')
-    })
-    new Promise(function(resolve) {
-        console.log('11')
-        resolve()
-    }).then(function() {
-        console.log('12')
-    })
-})
+console.log(111);
+setImmediate(function immediate () {
+  console.log('immediate');
+},0);
+setTimeout(function timeout () {
+  console.log('timeout');
+},0);
