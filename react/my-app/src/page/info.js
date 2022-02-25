9ac71b3c79f77aee230950d2b991f9e320bf6e8b
wuyurong <wuyurong@jsbdshop.cn>
2022-02-25 18:48:53 +0800

    自动测试

--- react/my-app/src/page/info.js
+++ react/my-app/src/page/info.js
@@ -1,15 +1,52 @@
 /*
  * @Author: 吴玉荣
  * @LastEditors: 吴玉荣
- * @Date: 2021-05-11 16:17:56
- * @LastEditTime: 2021-05-11 16:53:53
+ * @Date: 2021-12-06 14:15:33
+ * @LastEditTime: 2022-02-24 13:22:46
  * @info: 描述
  */
-import { Link } from 'react-router-dom'
-function info () {
-  return (
-    <div>info    <Link rel="stylesheet" to="/about">go about</Link> </div>
-  )
-}
+import React from 'react'
+import { createRef } from 'react'
+import MyContext from '../context/context'
+class Demo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 1,
+      refs: React.createRef()
+    }
+  }
+  btnClick(theme) {
+    theme.id = 3
+    this.setState({ count: this.state.count + 1 })
+    console.log(this.state);
+  }
+  componentDidMount() {
+    console.log(this.context, 'context');
 
-export default info
+  }
+  render() {
+    console.log(this.context, 'context');
+    return (
+      <MyContext.Consumer>
+        {theme => (
+          <div>
+            {this.state.count} {theme.id}
+            <FancyButton ref={this.state.refs}>哈哈</FancyButton>
+            <button onClick={() => { this.btnClick(theme) }}>btnClick</button>
+          </div>
+        )}
+      </MyContext.Consumer>
+    );
+  }
+}
+const FancyButton = React.forwardRef((props, ref) => (
+  <button ref={ref} className="FancyButton">
+    {props.children}
+  </button>
+));
+
+// 你可以直接获取 DOM button 的 ref：
+const ref = React.createRef();
+
+export default Demo
