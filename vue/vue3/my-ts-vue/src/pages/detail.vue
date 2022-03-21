42d050c8ea39a5b216bfb32a0c447aa21055bd8f
吴玉荣 <1065229722@qq.com>
2022-03-21 10:23:40 +0800

    自动测试

--- vue/vue3/my-ts-vue/src/pages/detail.vue
+++ vue/vue3/my-ts-vue/src/pages/detail.vue
@@ -1,7 +1,39 @@
 <template>
 	<div>
-		detail
+		<input v-model="text"  />
+		{{
+			text
+		}}
+		<button @click="clickBtn">点击</button>
 	</div>
 </template>
 <script setup lang="ts">
+import { log } from 'console'
+import { customRef } from 'vue'
+function useDebouncedRef(value, delay = 200) {
+  let timeout
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track()
+				console.log('获取值');
+				return 111
+        return value
+      },
+      set(newValue) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          value = newValue
+          trigger()
+        }, delay)
+      }
+    }
+  })
+}
+
+function clickBtn () {
+	console.log(text.value)
+}
+
+const text = useDebouncedRef('hello', 2000)
 </script>
