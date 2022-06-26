b52fc4d1f75317e17daf74ec1d90b1844b0398bc
djj <1065229722@qq.com>
2022-06-26 14:44:42 +0800

    自动测试

--- vue/vuetwo/src/router/index.js
+++ vue/vuetwo/src/router/index.js
@@ -57,6 +57,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/watch/watch.vue')
   },
+  {
+    path: '/table',
+    name: 'table',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/table/table.vue')
+  },
 ]
 
 const router = new VueRouter({
