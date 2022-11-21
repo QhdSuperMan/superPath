73f2b666856a92fac9bd6cd3d2626f650fc65d01
DESKTOP-JFRCGUH\Admin <1065229722@qq.com>
2022-11-21 23:51:57 +0800

    自动测试

--- vue/hello-two/src/components/HelloWorld.vue
+++ vue/hello-two/src/components/HelloWorld.vue
@@ -1,58 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="box">
+    <div
+      class="item"
+      v-for="(val, index) in drawBoxBig"
+      :key="index + 'big'"
+      :style="{ top: val.x, left: val.y }"
+    >
+      {{ index }}掘金
+    </div>
+    <div
+      class="item"
+      v-for="(val, index) in drawBox"
+      :key="index"
+      :style="{ top: val.x, left: val.y }"
+    >
+      {{ index }}掘金
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      box: [],
+      drawBox: [],
+      drawBoxBig: [],
+      r: 300,
+      rBig: 500,
+      center: { x: 600, y: 600 },
+    };
+  },
+  created() {
+    this.draw();
+
+    let that = this
+    window.addEventListener("resize", function () {
+      that.r = Math.floor((window.innerWidth / 2560) * 300);
+      that.rBig = Math.floor((window.innerWidth / 2560) * 500);
+      that.draw();
+    });
+  },
+  methods: {
+    draw() {
+      this.drawBox = this.drawCircle(new Array(8).fill(""), this.r, this.center);
+      this.drawBoxBig = this.drawCircle(new Array(25).fill(""), this.rBig, this.center, 270, 60);
+    },
+    /**
+     *
+     * @param {*} arr 数据
+     * @param {*} r  半径
+     * @param {*} center  中心点  x y坐标
+     * @param {*} allAngle  画的圆的总度数
+     * @param {*} startAndle  从初始度数开始画
+     */
+    drawCircle(arr, r, center, allAngle = 360, startAndle = 0) {
+      let angle = Math.floor(allAngle / arr.length);
+
+      return arr.map((val, index) => {
+        let itemAngle = index * angle + startAndle - 90;                                                                                                  
+        let x = center.x + r * Math.sin((itemAngle * 3.14) / 180);
+        let y = center.y + r * Math.cos((itemAngle * 3.14) / 180);
+
+        return {
+          x: Math.floor(x) + "px",
+          y: Math.floor(y) + "px",
+        };
+      });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+<style>
+* {
   padding: 0;
+  margin: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.box {
+  position: relative;
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
 }
-a {
-  color: #42b983;
+
+.item {
+  position: absolute;
+  color: white;
 }
 </style>
