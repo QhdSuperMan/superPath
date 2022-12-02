4ba3066a1a23eec198838c8e6a45b7e3b1710f4e
wuyurong <1065229722@qq.com>
2022-12-02 14:11:55 +0800

    自动测试

---
+++ share.bat
@@ -0,0 +1,14 @@
+net share c:\ /delete
+echo 'delete c:\'
+
+net share c:\Windows /delete
+echo 'delete c:\Windows'
+
+net share d:\ /delete
+echo 'delete d:\'
+
+net share e:\ /delete
+echo 'delete e:\'
+
+
+pause
